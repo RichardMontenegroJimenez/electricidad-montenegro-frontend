7cb0dada8b320dd1a68b34d94c4d1184f238390f
Richard <1903023@alu.murciaeduca.es>
2022-06-12 02:46:20 +0200

    Implementado contacto para emmpresas con formulario, vistas y login, resultado final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,8 @@ import { AuthGuard } from './usuarios/guards/auth.guard';
 import { RoleGuard } from './usuarios/guards/role.guard';
 import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
 import { ContactaComponent } from './contacta/contacta.component';
+import { EmpresasComponent } from './empresas/empresas.component';
+import { FormEmpresasComponent } from './empresas/form-empresas/form-empresas.component';
 
 
 
@@ -38,12 +40,14 @@ const routes: Routes = [
   {path: 'obras', component: ObrasComponent},
   {path: 'obras/form', component: FormObrasComponent, canActivate:[AuthGuard, RoleGuard], data: {role: ['ROLE_ADMIN']}},
   {path: 'obras/form/:id', component: FormObrasComponent, canActivate:[AuthGuard, RoleGuard], data: {role: ['ROLE_ADMIN']}},
-  {path: 'encargados', component: EncargadosComponent},
+  {path: 'encargados', component: EncargadosComponent, canActivate:[AuthGuard, RoleGuard], data: {role: ['ROLE_ADMIN', 'ROLE_ENCARGADO', 'ROLE_EMPLEADO']}},
   {path: 'encargados/form', component: FormEncargadosComponent, canActivate:[AuthGuard, RoleGuard], data: {role: ['ROLE_ADMIN']}},
   {path: 'encargados/form/:id', component: FormEncargadosComponent, canActivate:[AuthGuard, RoleGuard], data: {role: ['ROLE_ADMIN']}},
-  {path: 'empleados', component: EmpleadosComponent},
+  {path: 'empleados', component: EmpleadosComponent, canActivate:[AuthGuard, RoleGuard], data: {role: ['ROLE_ADMIN', 'ROLE_ENCARGADO', 'ROLE_EMPLEADO']}},
   {path: 'empleados/form', component: FormEmpleadosComponent, canActivate:[AuthGuard, RoleGuard], data: {role: ['ROLE_ADMIN', 'ROLE_ENCARGADO']}},
   {path: 'empleados/form/:id', component: FormEmpleadosComponent, canActivate:[AuthGuard, RoleGuard], data: {role: ['ROLE_ADMIN', 'ROLE_ENCARGADO']} },
+  {path: 'empresas', component: EmpresasComponent, canActivate:[AuthGuard, RoleGuard], data: {role: ['ROLE_ADMIN', 'ROLE_ENCARGADO', 'ROLE_EMPLEADO']}},
+  {path: 'empresas/form', component: FormEmpresasComponent},
   {path: 'login', component: LoginComponent}
 ];
 
@@ -64,7 +68,9 @@ const routes: Routes = [
     PerfilEmpleadoComponent,
     LoginComponent,
     QuienesSomosComponent,
-    ContactaComponent
+    ContactaComponent,
+    EmpresasComponent,
+    FormEmpresasComponent
   ],
   imports: [
     BrowserModule,
